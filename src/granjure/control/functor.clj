e9441b8009938aac06353559b0528f52017871e1
pasberth <pasberth@gmail.com>
2013-05-07 06:56:41 +0900

    Functor も抽象的なデータ構造にしないと無理っぽい。 fmap f (Unit a) とか fmap f (Pure a) とかされると死ぬ。

--- src/granjure/control/functor.clj
+++ src/granjure/control/functor.clj
@@ -1,10 +1,21 @@
 (ns granjure.control.functor
   (:use granjure.core
-        granjure.primitive))
+        granjure.primitive
+        granjure.control))
 
 (defprotocol Functor
   (commutated-fmap [functor f]))
 
-(def fmap (cfn [f functor] (commutated-fmap functor f)))
+(defrecord Fmap [f fr])
+
+(extend-protocol TypeClass
+  Fmap
+    (infer-context [this] (infer-context (:fr this)))
+    (specialize [fr cxt] (commutated-fmap (specialize (:fr fr) cxt) (:f fr))))
+
+(def fmap (cfn [f fr]
+  (let [cxt (infer-context fr)] (if cxt
+    (specialize (Fmap. f fr) cxt)
+    (Fmap. f fr)))))
 (def <$> fmap)
 (def <$  (compose <$> const))
