9b90f5e4744a66c74528871d1c537c90490aa456
pasberth <pasberth@gmail.com>
2013-05-21 13:28:42 +0900

    tuple の定義変更。 Tuple は List としても扱えるようにするが、 List とは別の Functor を持つ。また、 tuple の要素にアクセスする場合は、他の list と同じように first second などを使う。

--- src/granjure/data/tuple.clj
+++ src/granjure/data/tuple.clj
@@ -1,11 +1,16 @@
 (ns granjure.data.tuple
   (:use granjure.primitive))
 
-(def tuple2 (cfn [a b] (list a b)))
-(def tuple3 (cfn [a b c] (list a b c)))
-(def tuple4 (cfn [a b c d] (list a b c d)))
-(def tuple5 (cfn [a b c d e] (list a b c d e)))
-(def tuple6 (cfn [a b c d e f] (list a b c d e f)))
-(def tuple7 (cfn [a b c d e f g] (list a b c d e f g)))
-(def tuple8 (cfn [a b c d e f g h] (list a b c d e f g h)))
-(def tuple9 (cfn [a b c d e f g h i] (list a b c d e f g h i)))
+(defrecord Tuple [xs])
+
+(defn tuple [& xs]
+  (Tuple. xs))
+
+(def tuple2 (cfn [a b] (tuple a b)))
+(def tuple3 (cfn [a b c] (tuple a b c)))
+(def tuple4 (cfn [a b c d] (tuple a b c d)))
+(def tuple5 (cfn [a b c d e] (tuple a b c d e)))
+(def tuple6 (cfn [a b c d e f] (tuple a b c d e f)))
+(def tuple7 (cfn [a b c d e f g] (tuple a b c d e f g)))
+(def tuple8 (cfn [a b c d e f g h] (tuple a b c d e f g h)))
+(def tuple9 (cfn [a b c d e f g h i] (tuple a b c d e f g h i)))
