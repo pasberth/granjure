9d72dc71a4e5095d73b602869573958ae95ca722
pasberth <pasberth@gmail.com>
2013-05-23 02:30:57 +0900

    Either をモナドにした。

--- src/granjure/data/either.clj
+++ src/granjure/data/either.clj
@@ -1,5 +1,8 @@
 (ns granjure.data.either
+  (:refer-clojure :exclude [when])
   (:use granjure.primitive
+        granjure.control.functor
+        granjure.control.monad
         granjure.data))
 
 (defmacro eithern [n]
@@ -18,6 +21,20 @@
     (invoke [_ f] ((eithern 2) a f))
     (invoke [_ f g] ((eithern 2) a f g))
     (applyTo [_ args] (apply (eithern 2) a args)))
+
+(extend-protocol Functor
+  Left
+    (commutated-fmap [this _] this)
+  Right
+    (commutated-fmap [this f] (Right. (f (.a this)))))
+
+(extend-protocol Monad
+  Left
+    (unit [_ a] (Right. a))
+    (bind [m _] m)
+  Right
+    (unit [_ a] (Right. a))
+    (bind [m k] (k (.a m))))
 
 (def left (cfn [f e] (e (cfn [a] (Left. (f a))) (cfn [a] (Right. a)))))
 (def right (cfn [f e] (e (cfn [a] (Left. a)) (cfn [a] (Right. (f a))))))
