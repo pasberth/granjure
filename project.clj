17bf692ca3913d01bef0f274d4b960f643d1cb45
pasberth <pasberth@gmail.com>
2013-07-08 23:59:48 +0900

    静的片付けのアレ作り直す

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/pasberth/granjure"
   ; :license {}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.match "0.2.0-rc3"]
                  [infixing "0.1.0"]])
---
+++ src/granjure/statically/typed.clj
@@ -0,0 +1,44 @@
+(ns granjure.statically.typed
+  (:use [clojure.core.match :only (match)]))
+
+(defrecord TypeSystem [ast cxt])
+(defrecord &&&        [first second])
+(defrecord |||        [left right])
+(defrecord Arr        [src tgt])
+(defrecord Var        [sym])
+
+(defn assume [type-system sym type]
+  (TypeSystem. (:ast type-system) (conj (:cxt type-system) [sym type])))
+
+(declare syntactic-type)
+
+(defn statically-type [type-system ast] (cond 
+  (seq?    ast) (syntactic-type type-system ast)
+  (symbol? ast) ((:cxt type-system) ast)
+  :else         (type ast)))
+
+(defmulti  syntactic-type                 (fn [type-system ast] (first ast)))
+(defmethod syntactic-type 'fn*                [type-system ast] (letfn
+  [ (typing [params expr] (let
+      [ src        (reduce (fn [a b] (&&&. a b)) (reverse params))
+      , assumption (reduce (fn [a b] (assume a b (Var. b))) type-system params)
+      , tgt        (statically-type assumption expr) ]
+      (Arr. src tgt)))
+  ] (match [(count ast)]
+    [2] (let [ [ _ params-expr-pair ] ast ] (apply typing params-expr-pair))
+    [3] (cond
+      (symbol? (fnext ast)) (let [ [ _ name params-expr-pair ] ast ] (apply typing params-expr-pair))
+      (vector? (fnext ast)) (let [ [ _ params expr ]           ast ] (typing params expr))
+      (seq?    (fnext ast)) (let [ [ _ px-pair1 px-pair2 ]     ast ] (|||. (apply typing px-pair1) (apply typing px-pair2)))
+      :else                 (throw (IllegalArgumentException.)))
+    :else (throw (IllegalArgumentException.)))))
+(defmethod syntactic-type clojure.lang.Symbol [type-system ast] (let
+  [ ast'   (macroexpand ast)
+  , macro? (not= ast ast')
+  , apply? (not macro?)
+  ] (letfn
+  [ (syntactic-type-macro [] (syntactic-type  type-system ast'))
+  , (syntactic-type-apply [] nil)
+  ] (cond
+    macro? (syntactic-type-macro)
+    apply? (syntactic-type-apply)))))
