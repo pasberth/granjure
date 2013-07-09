ec7fb036c8b99167ad0e8dd0d7b94b527110cb08
pasberth <pasberth@gmail.com>
2013-07-09 11:21:57 +0900

    Seqable に List とか代入できる

--- src/granjure/statically/typed.clj
+++ src/granjure/statically/typed.clj
@@ -59,14 +59,18 @@
 (defmethod replace-variable ||| [t src dest] (|||. (replace-variable (:left t) src dest) (replace-variable (:right t) src dest)))
 (defmethod replace-variable Arr [t src dest] (Arr. (replace-variable (:src t) src dest) (replace-variable (:tgt t) src dest)))
 (defmethod replace-variable Var [t src dest] (if (= src t) dest t))
-(defmethod replace-variable Class [t src dest] t)
+(defmethod replace-variable Class [t src dest] (if (= src t) dest t))
 
 (defmulti  subst-variable          (fn [t src dest] [(type src), (type dest)]))
 (defmethod subst-variable [||| Object] [t src dest] (or (subst-variable t (:left src) dest) (subst-variable t (:right src) dest)))
 (defmethod subst-variable [Object |||] [t src dest] (or (subst-variable t src (:left dest)) (subst-variable t src (:right dest))))
 (defmethod subst-variable [&&& &&&]    [t src dest] (subst-variable (subst-variable t (:first src) (:first dest)) (:second src) (:second dest)))
 (defmethod subst-variable [Arr Arr]    [t src dest] (subst-variable (subst-variable t (:src src) (:src dest)) (:tgt src) (:tgt dest)))
+(defmethod subst-variable [Var Var]    [t src dest] t)
 (defmethod subst-variable [Var Object] [t src dest] (replace-variable t src dest))
+(defmethod subst-variable [Class Class] [t src dest] (cond
+  (contains? (ancestors dest) src) (replace-variable t src dest)
+  :else                            nil))
 (defmethod subst-variable :default     [t src dest] nil)
 
 (defn rename-conflicting-variables [t r]
