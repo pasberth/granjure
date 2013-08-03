b399936b86007d8b7d47f40dfa075d868e583d68
pasberth <pasberth@gmail.com>
2013-08-03 18:51:20 +0900

    fixr, fixlr 定義。あと test 追加。

--- src/granjure/syntax.clj
+++ src/granjure/syntax.clj
@@ -5,6 +5,8 @@
            variables (set variables)
          ]
   (fn [tokens] (cond
+    (not (seq? tokens))
+      tokens
     (< (count tokens) len)
       tokens
     :else (let [ tokens' (drop len tokens)
@@ -15,3 +17,13 @@
         (cons (apply format tokens) tokens')
       :else
         (concat tokens tokens')))))))
+
+(defn fixr [formula tokens]
+  (reduce (fn [tokens i] (cond (> i (count tokens)) tokens :else (concat (take i tokens) (formula (drop i tokens)))))
+          tokens
+          (flatten (reverse (map #(repeat (inc %) %) (range (count tokens)))))))
+
+(defn fixl [formula tokens]
+  (reduce (fn [tokens i] (cond (> i (count tokens)) tokens :else (concat (take i tokens) (formula (drop i tokens)))))
+          tokens
+          (flatten (map #(repeat (- (count tokens) %) %) (range (count tokens))))))
---
+++ test/granjure/syntax_test.clj
@@ -0,0 +1,33 @@
+(ns granjure.syntax_test
+  (:use clojure.test
+        granjure.syntax))
+
+
+(def add-rule (forall (fn [a b c] (list b a c)) #{:a :b} '(:a + :b)))
+(def sub-rule (forall (fn [a b c] (list b a c)) #{:a :b} '(:a - :b)))
+(def mul-rule (forall (fn [a b c] (list b a c)) #{:a :b} '(:a * :b)))
+(def div-rule (forall (fn [a b c] (list b a c)) #{:a :b} '(:a / :b)))
+
+(deftest ret-literal1
+  (testing "x == x"
+    (is (= (add-rule 'x) 'x))))
+
+(deftest ret-literal2
+  (testing "(x) == (x)"
+    (is (= (add-rule '(x)) '(x)))))
+
+(deftest add-rule-test1
+  (testing "(x + y) == ((+ x y))"
+    (is (= (add-rule '(x + y)) '((+ x y))))))
+
+(deftest add-rule-test1
+  (testing "(x + y + z) == ((+ x y) + z)"
+    (is (= (add-rule '(x + y + z)) '((+ x y) + z)))))
+
+(deftest fixl-add-rule-test
+  (testing "(x + y + z) == ((+ (+ x y) z))"
+    (is (= (fixl add-rule '(x + y + z)) '((+ (+ x y) z))))))
+
+(deftest fixr-add-rule-test
+  (testing "(x + y + z) == ((+ x (+ y z)))"
+    (is (= (fixr add-rule '(x + y + z)) '((+ x (+ y z)))))))
